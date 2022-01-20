00e713a69cb8497c7d38de7064d5bc8f86d6487e
alegomezram <alegomezram@unal.edu.co>
2022-01-20 18:43:35 -0500

    display the 2D surfaces

--- Algorithm.py
+++ Algorithm.py
@@ -5,14 +5,12 @@ import numpy as np
 import time
 import cv2
 
-w_length=0.633 #um
 
 xx=np.linspace(-np.pi,np.pi,num=1024)
 yy=np.linspace(-np.pi,np.pi,num=1024)
 x,y=np.meshgrid(xx,yy)
 
 r=x**2 +y**2
-k=2*np.pi/w_length
 z=(1/r)*np.exp(1j*r)
 
 phase=np.angle(z)     #wrapped phase
@@ -20,13 +18,15 @@ phase=np.angle(z)     #wrapped phase
 # Plot the surface
 fig = plt.figure(1)
 ax = plt.axes(projection='3d')
-surf = ax.plot_surface(x, y, phase,rstride=4, cstride=4,cmap='viridis', edgecolor='none')
+surf = ax.plot_surface(x, y, phase,rstride=5, cstride=5,cmap='viridis', edgecolor='none')
 plt.savefig("phasewrap.png")
+plt.imsave("phasewrap2D.png",phase, cmap='gray') 
 
 realphase=np.unwrap(phase)
 
 # Plot the surface
 fig = plt.figure(2)
 ax = plt.axes(projection='3d')
-surf = ax.plot_surface(x, y, realphase,rstride=4, cstride=4,cmap='viridis', edgecolor='none')
+surf = ax.plot_surface(x, y, realphase,rstride=5, cstride=5,cmap='viridis', edgecolor='none')
 plt.savefig("phaseunwrap.png")
+plt.imsave("phaseunwrap2D.png",realphase, cmap='gray') 
