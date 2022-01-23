fef24aa67ae039e92e284c86bc574dba70dbf167
alegomezram <alegomezram@unal.edu.co>
2022-01-22 19:09:55 -0500

    Constructing the algorithm for reconstruction
    The relative phase and absolute phase functions are added. Also different options for the 3D reconstructions are added.

--- Algorithm.py
+++ Algorithm.py
@@ -5,28 +5,71 @@ import numpy as np
 import time
 import cv2
 
+#Functions
 
-xx=np.linspace(-np.pi,np.pi,num=1024)
-yy=np.linspace(-np.pi,np.pi,num=1024)
-x,y=np.meshgrid(xx,yy)
+def unwraping(phase):
 
-r=x**2 +y**2
-z=(1/r)*np.exp(1j*r)
+	#phase unwrapping
 
-phase=np.angle(z)     #wrapped phase
+	absolutephase=(np.unwrap(np.unwrap(phase,axis=0), axis=1))
 
-# Plot the surface
+	return absolutephase
+
+
+def 3D(phaseobj,phaseref,l,d,f):
+
+	#Depth information acquisition
+
+	deltaph=phaseobj-phaseref   #phase difference
+
+	z=(l/d)*(deltaph)  #option1
+
+	z=(l*deltaph)/(deltaph+(2*np.pi*d*f))  #option2
+
+	z=z0+c0*(deltaph)   #option3
+
+	return z
+
+
+#Import pictures of object
+I1= cv2.imread("I1.png",0)  #Intensity of fringe pattern with phase shift=-2pi/3
+I2= cv2.imread("I2.png",0)  #Intensity of fringe pattern with phase shift=0
+I3= cv2.imread("I3.png",0)  #Intensity of fringe pattern with phase shift=+2pi/3
+
+#Import pictures of reference plane
+I1r= cv2.imread("I1r.png",0)  #Intensity of fringe pattern with phase shift=-2pi/3
+I2r= cv2.imread("I2r.png",0)  #Intensity of fringe pattern with phase shift=0
+I3r= cv2.imread("I3r.png",0)  #Intensity of fringe pattern with phase shift=+2pi/3
+
+#System setup values
+l=1 #(ejemplo de valor)  #Distance between camera/projector plane and reference plane
+d=1   #Distance between camera and projector
+f=1   #Frequency of the projected fringe pattern
+
+#Object
+eq=np.sqrt(3)*((I1-I3)/(2*I2-I1-I3))
+phase=np.arctan(eq)         #Relative phase calculation
+realphase_ob=unwraping(phase)  #Absolute phase calculation
+
+#Reference plane
+eq_r=np.sqrt(3)*((I1r-I3r)/(2*I2r-I1r-I3r))
+phase_r=np.arctan(eq_r)         #Relative phase calculation
+realphase_r=unwraping(phase_r)  #Absolute phase calculation
+
+#3D object reconstruction
+z=3D(realphase_ob,realphase_r,l,d,f)
+
+
+
+
+
+# Plots
 fig = plt.figure(1)
-ax = plt.axes(projection='3d')
-surf = ax.plot_surface(x, y, phase,rstride=5, cstride=5,cmap='viridis', edgecolor='none')
-plt.savefig("phasewrap.png")
-plt.imsave("phasewrap2D.png",phase, cmap='gray') 
+plt.imsave("phasewrap2D.png",phase, cmap='gray')  #2D plot of wrapped phase
 
-#realphase=np.unwrap(phase)/2
-realphase=(np.unwrap(np.unwrap(phase,axis=0), axis=1))
-# Plot the surface
+
 fig = plt.figure(2)
 ax = plt.axes(projection='3d')
 surf = ax.plot_surface(x, y, realphase,rstride=5, cstride=5,cmap='viridis', edgecolor='none')
-plt.savefig("phaseunwrap.png")
-plt.imsave("phaseunwrap2D.png",realphase, cmap='gray') 
+plt.savefig("phaseunwrap.png")                         #3D plot of unwrapped phase
+plt.imsave("phaseunwrap2D.png",realphase, cmap='gray') #2D plot of unwrapped phase
